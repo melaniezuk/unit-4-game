df2b6487e533e1f1dc1595277842d7b7a6a3162d
MelZee <zenani3@gmail.com>
2018-05-17 10:52:43 -0400

    revision2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,3 +23,55 @@ var userTotal= 0;
 var wins= 0;
 var losses= 0;
 
+/*resets the game*/
+funtion reset (){
+       
+    $('#randomNumber').text(Random);
+       
+    var gemgreen=Math.floor(Math.random()*11+1)
+    var gemred=Math.floor(Math.random()*11+1)
+    var gemyellow=Math.floor(Math.random()*11+1)
+    var gemdiamond=Math.floor(Math.random()*11+1)
+       
+    }
+
+    /*adds wins to the user total*/
+
+    var win = 0;
+
+    function myFunction1() {
+    if (test3.innerHTML == "You win!") {
+
+    win = win + 1;
+
+    /*adds the losses to the userTotal*/
+
+    var lose = 0;
+
+    function myFunction1() {
+    then (test3.innerHTML == "You lose! :-(") {
+
+    lose = lose + 1;
+
+    /*sets up click for jewels*/
+
+    $('#one').on ('click', function(){
+    userTotal = userTotal + num1;
+    console.log("New userTotal= " + userTotal);
+   
+    $('#two').on ('click', function(){
+    userTotal = userTotal + num2;
+    console.log("New userTotal= " + userTotal);
+
+    $('#three').on ('click', function(){
+    userTotal = userTotal + num3;
+    console.log("New userTotal= " + userTotal);
+
+    $('#four').on ('click', function(){
+    userTotal = userTotal + num4;
+    console.log("New userTotal= " + userTotal);
+
+    </script>
+
+    
+
